b3a1ba8c9b7d1b93c2ec60306d51ccff97535375
mjha17 <36537048+mjha17@users.noreply.github.com>
2019-04-25 11:38:28 +0200

    Flipped if-statement NOW WORKS up to 2oo hz??

--- Synthesis/KarplusStrong.py
+++ Synthesis/KarplusStrong.py
@@ -7,7 +7,7 @@ CHUNK = SAMPLERATE * 4
 
 def karplusStrongChunk(frequency):
 
-    combParameter = 0.9
+    combParameter = 0.94
     pitchPeriod = SAMPLERATE/frequency
     combDelay = m.floor(pitchPeriod - 0.5)
 
@@ -15,14 +15,14 @@ def karplusStrongChunk(frequency):
     apParameter = (1-d)/(1+d)
 
     output = np.zeros(CHUNK)
-    input = np.r_[np.random.randn(200),np.zeros(CHUNK-200)]
+    input = np.r_[np.random.randn(440),np.zeros(CHUNK-440)]
 
     combPrev = 0
     lpPrev = 0
 
     for n in np.arange(CHUNK):
 
-        if(combDelay < n):
+        if(combDelay > n):
             comb = input[n]
         else:
             comb = input[n] + combParameter*output[n-combDelay]
