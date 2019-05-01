40467667bb0d2853d707d1169898d062e4187933
Stefan Nordborg Eriksen <the-grey-side@hotmail.com>
2019-05-01 15:41:12 +0200

    Various modifications to input device

--- Arduino/InputDevice/InputDevice.ino
+++ Arduino/InputDevice/InputDevice.ino
@@ -1,9 +1,14 @@
-int input = A14;
-int filteredInput = A15;
-int IRin = A13; 
+#include <SharpIR.h>
+
+#define IN A14
+#define FILTERED_IN A15
+#define IR A13 // define IR signal pin
+#define model 1080 // used 1080 because model GP2Y0A21YK0F is used
 
 //Filter used was 400K ohm (4x100k in series), 104pF
 //We might want to consider using a potentiometer in the setup, so we can adjust it more granuarily
+
+SharpIR SharpIR(IR, model);
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,8 +17,16 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print("Input: "); Serial.print(analogRead(input)); Serial.print("  ");
-  Serial.print("Filtered: "); Serial.print(analogRead(filteredInput)); Serial.print("  ");
-  Serial.print("IR: "); Serial.print(analogRead(IRin)); Serial.print("  ");
+  unsigned long startTime = millis(); // takes the time before the loop on the library begins
+  int dis = SharpIR.distance();  // this returns the distance to the object you're measuring
+  dis = constrain(dis, 10, 80);
+
+  Serial.print("Input: "); Serial.print(analogRead(IN)); Serial.print("  ");
+  Serial.print("Filtered: "); Serial.print(analogRead(FILTERED_IN)); Serial.print("  ");
+  Serial.print("IR: "); Serial.print(dis); Serial.print("  ");
   Serial.println("uT");
+
+
+
+  unsigned long endTime=millis()-startTime;  // the following gives you the time taken to get the measurement     
 }
